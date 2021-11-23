ec8868dc5e4c192172b43621ca7f82d1d18df02c
Antas-Sharma36 <sharmaantas20@gmail.com>
2021-11-24 00:16:49 +0530

    Added Flexbox

---
+++ about_page/script.js
@@ -0,0 +1,4 @@
+function run_on_load() {
+    document.querySelector(".content .display_contact").innerHTML = "hello";
+}
+window.onload = run_on_load();
--- home_page/script.js
+++ home_page/script.js
@@ -1,3 +1,7 @@
-function know_more() {
+function contact_us() {
+    window.location.href = "../bmi_page/index.html";
+}
+
+function sign_in() {
     window.location.href = "../bmi_page/index.html";
 }
